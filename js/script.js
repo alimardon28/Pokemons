e0237cb918349b36b5d34d96cf6d43f135691f03
alimardon28 <doniyorovalimardon369@gmail.com>
2022-01-10 17:05:13 +0500

    added heart ,border-bottom and border

--- js/script.js
+++ js/script.js
@@ -11,6 +11,8 @@ let newItem = document.createElement('li');
 let newCard = document.createElement('div');
 let newImg = document.createElement('img');
 let newCardBody = document.createElement('div');
+let newPokemonsHeart = document.createElement('div');
+let newPokemonsHeartImg = document.createElement('img');
 let newCardTitle = document.createElement('h5');
 let newPokemonsTitle = document.createElement('p');
 let newPokemonsTime = document.createElement('p');
@@ -22,12 +24,19 @@ let newPokemonsAge = document.createElement('span');
 
 newItem.setAttribute('class' ,'pokemons__item');
 newCard.setAttribute('class' , 'card pokemons__card border my-3');
-newImg.setAttribute('class', ' card-img-top border-bottom border-dark border-2');
+newImg.setAttribute('class', ' card-img-top pb-5 pt-3');
 newImg.setAttribute('src', pokemon.img);
+newImg.setAttribute('width' , '157');
+newImg.setAttribute('height' , '157');
 newCardBody.setAttribute('class' , 'card-body');
+newPokemonsHeart.setAttribute('class' , 'pokemons-heart');
+newPokemonsHeartImg.setAttribute('class' , 'pokemons-heart-img');
 newCardTitle.setAttribute('class' , 'card-title fw-bold fs-5');
+newPokemonsHeartImg.setAttribute('src' , './img/heart.svg');
+newPokemonsHeartImg.setAttribute('width' , '30');
+newPokemonsHeartImg.setAttribute('height', '30');
 newPokemonsTitle.setAttribute('class' , 'pokemons__height fw-bold');
-newPokemonsTime.setAttribute('class' , 'pokemons__time fw-bold')
+newPokemonsTime.setAttribute('class' , 'pokemons__time fw-bold');
 newPokemonsMass.setAttribute('class' , 'pokemons__weight me-3 fw-bold fs-5');
 newPokemonsAge.setAttribute('class' , 'pokemons__age fw-bold fs-5');
 
@@ -43,6 +52,7 @@ newCard.appendChild(newImg);
 newCard.appendChild(newCardBody);
 newCard.appendChild(newCardBody);
 newCardBody.appendChild(newCardTitle);
+newCardBody.appendChild(newPokemonsHeartImg);
 newCardBody.appendChild(newPokemonsTitle);
 newCardBody.appendChild(newPokemonsTime);
 newCardBody.appendChild(newPokemonsMass);
